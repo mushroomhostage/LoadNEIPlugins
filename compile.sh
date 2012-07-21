8446df47aed2bd7f5f4efb61db5fa2305b90a5bc
Mushroom Hostage <mushroomhostage@yahoo.com>
2012-07-20 22:56:03 -0700

    Force loading NEI plugins for ForgeOre, Forestry, RailCraft, and RedPower

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-javac -cp minecraftforge-client-3.3.8.152.zip mod_LoadNEIPlugins.java
+javac -cp minecraftforge-client-3.3.8.152.zip:NEI_ForestryPlugin\ 1.0.5.17.zip:NEI_RailCraftPlugin\ 1.2.2.1.zip:NEI_RedPowerPlugin\ 1.2.2.3.zip:CodeChickenCore-Client\ 0.5.3.zip:NotEnoughItems-Client\ 1.2.2.4.zip mod_LoadNEIPlugins.java
 zip -r LoadNEIPlugins.zip *.class
 
--- mod_LoadNEIPlugins.java
+++ mod_LoadNEIPlugins.java
@@ -1,4 +1,6 @@
 
+import codechicken.nei.IConfigureNEI;
+
 public class mod_LoadNEIPlugins extends BaseMod {
     public String getVersion() {
         return "1.2.5";
@@ -6,9 +8,75 @@ public class mod_LoadNEIPlugins extends BaseMod {
 
     public void load() {
         System.out.println("Loading NEI plugins");
-    }
+
+        // TODO: configurable
+        try {
+            (new codechicken.nei.plugins.forge.NEIForgeOreConfig()).loadConfig();
+            System.out.println("Loaded codechicken.nei.plugins.forge.NEIForgeOreConfig");
+        } catch (Exception e) {
+            System.out.println("Not loading codechicken.nei.plugins.forge.NEIForgeOreConfig");
+        }
+
+
+        try {
+            (new codechicken.nei.plugins.forestry.NEIForestryConfig()).loadConfig();
+            System.out.println("Loaded codechicken.nei.plugins.forestry.NEIForestryConfig");
+        } catch (Exception e) {
+            System.out.println("Not loading codechicken.nei.plugins.forestry.NEIForestryConfig");
+        }
+
+
+        try {
+            (new codechicken.nei.plugins.railcraft.NEIRailCraftConfig()).loadConfig();
+            System.out.println("Loaded codechicken.nei.plugins.railcraft.NEIRailCraftConfig");
+        } catch (Exception e) {
+            System.out.println("Not loading codechicken.nei.plugins.railcraft.NEIRailCraftConfig");
+        }
+
+        try {
+            (new codechicken.nei.plugins.redpower.NEIRedPowerConfig()).loadConfig();
+            System.out.println("Loaded codechicken.nei.plugins.redpower.NEIRedPowerConfig");
+        } catch (Exception e) {
+            System.out.println("Not loading codechicken.nei.plugins.redpower.NEIRedPowerConfig");
+        }
+
+        System.out.println("Finished loading NEI plugins");
+   }
 
     public String getPriorities() {
         return "after:*";
     }
+
+    /* see NEIConfig
+   public static void loadConfig() {
+      if(!configLoaded) {
+         loadSavedConfig();
+         vishash = new ItemVisibilityHash();
+         LayoutManager.searchField.text = getSearchExpression();
+         ItemInfo.load();
+         configLoaded = true;
+         ArrayList configclasses = CommonUtils.findClasses(new IStringMatcher() {
+            public boolean matches(String test) {
+               return test.startsWith("NEI") && test.endsWith("Config.class");
+            }
+         }, new Class[]{IConfigureNEI.class});
+         Iterator var2 = configclasses.iterator();
+
+         while(var2.hasNext()) {
+            Class class1 = (Class)var2.next();
+
+            try {
+               IConfigureNEI e = (IConfigureNEI)class1.newInstance();
+               e.loadConfig();
+               System.out.println("Loaded " + class1.getName());
+            } catch (Exception var4) {
+               System.out.println("Failed to Load " + class1.getName());
+               var4.printStackTrace();
+            }
+         }
+
+      }
+    }
+  */
+
 }
